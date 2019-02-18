41e5957801e9e46339f1c24a37f63750287d605e
EC2 Default User <ec2-user@ip-172-31-25-9.ec2.internal>
2019-02-18 12:52:53 +0000

    ciao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
 export class AppComponent {
-  title = 'hellohhh';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+     let t = title.value;
+     let v = link.value;
+    console.log(`Adding article title: `+ t + ` and link: `+ v);
+    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
 
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent,
+    UserListComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,  NgbModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
